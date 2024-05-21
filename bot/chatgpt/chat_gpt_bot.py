02f2e646bc407db423caad73e2a8da688a9df609
Ericwangjianqi <ericwangjianqi@163.com>
2024-05-21 19:21:08 +0800

    '加入对输入信息的判定'

--- bot/chatgpt/chat_gpt_bot.py
+++ bot/chatgpt/chat_gpt_bot.py
@@ -81,36 +81,46 @@ class ChatGPTBot(Bot, OpenAIImage):
                 lines = f.readlines()
 
                 for i, line in enumerate(lines):
-                    introduction_list.append(line.strip().split("\t")[0])
-                    link_list.append(line.strip().split("\t")[1])
-            length = len(introduction_list)
-            question = query
-            faq = " ".join(introduction_list)
-            # query = '请问 “' + question + '” 与下面1和2哪个结果更匹配,如果都不匹配则请你给出一个答案。 1. 线上打车的流程与方法 2. 线上挂号的流程与方法'
-            # query = "请问“如何线上订餐”与下面5个选项中的哪一个更匹配， 1. 线上打车的教程。 2.线上挂号指南。3. 如何进行人脸验证。 4. 社保资格年度认证教程。5. 完全不相关。"
-            query = f'请问 "{question}" 与下面{length}个选项中的哪一个更匹配, {faq}。'
+                    introduction_list.append(line.strip().split(" ")[0])
+                    link_list.append(line.strip().split(" ")[1])
+            # length = len(introduction_list)
+            # question = query
+            # faq = " ".join(introduction_list)
+            # # query = '请问 “' + question + '” 与下面1和2哪个结果更匹配,如果都不匹配则请你给出一个答案。 1. 线上打车的流程与方法 2. 线上挂号的流程与方法'
+            # # query = "请问“如何线上订餐”与下面5个选项中的哪一个更匹配， 1. 线上打车的教程。 2.线上挂号指南。3. 如何进行人脸验证。 4. 社保资格年度认证教程。5. 完全不相关。"
+            # query = f'请问 "{question}" 与下面{length}个选项中的哪一个更匹配, 请给出选项的序号，并给出你的答案，{faq}'
             if len(session.messages) > 2:
                 session.messages = session.messages[:2]
             session.messages[1]["content"] = query
-            session.messages[0]["content"] = "你是一个智能助手，你可以帮助人们发现信息，回答问题。"
+            # session.messages[0]["content"] = "你是一个智能助手，你可以帮助人们发现信息，回答问题。"
             reply_content = self.reply_text(session, api_key, args=new_args)
-            str = reply_content["content"]
-            if "线上打车的教程" in str:
+            if(query == "1" or query == "一"):
                 reply_content["content"] = link_list[0]
-            elif "线上挂号指南" in str:
+            if(query == "2" or query == "二"):
                 reply_content["content"] = link_list[1]
-            elif "如何进行人脸验证" in str:
+            if(query == "3" or query == "三"):
                 reply_content["content"] = link_list[2]
-            elif "社保资格年度认证教程" in str:
+            if(query == "4" or query == "四"):
                 reply_content["content"] = link_list[3]
-            elif "线上订餐教程" in str:
+            if(query == "5" or query == "五"):
                 reply_content["content"] = link_list[4]
-            else:
-                query = question
-                if len(session.messages) > 2:
-                    session.messages = session.messages[:2]
-                session.messages[1]["content"] = query
-                reply_content = self.reply_text(session, api_key, args=new_args)
+            # str = reply_content["content"]
+            # if "线上打车的教程" in str:
+            #     reply_content["content"] = link_list[0]
+            # elif "线上挂号指南" in str:
+            #     reply_content["content"] = link_list[1]
+            # elif "如何进行人脸验证" in str:
+            #     reply_content["content"] = link_list[2]
+            # elif "社保资格年度认证教程" in str:
+            #     reply_content["content"] = link_list[3]
+            # elif "线上订餐教程" in str:
+            #     reply_content["content"] = link_list[4]
+            # else:
+            #     query = question
+            #     if len(session.messages) > 2:
+            #         session.messages = session.messages[:2]
+            #     session.messages[1]["content"] = query
+            #     reply_content = self.reply_text(session, api_key, args=new_args)
             
             logger.debug(
                 "[CHATGPT] new_query={}, session_id={}, reply_cont={}, completion_tokens={}".format(
